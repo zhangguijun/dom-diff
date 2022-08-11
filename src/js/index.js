69f30195775cdf9021a4562ab8243a91bfcdcdca
zhangguijun <zhangguijun@qq.com>
2022-08-11 11:31:38 +0800

    真实dom 打补丁

--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,79 @@
-console.log('111')
-
-
-import { createElement, render, renderDOM } from './vDom'
-
+import { createElement, render, renderDOM } from './vDom';
 import domDiff from './domDiff';
-const vDom = createElement('ul', {
-    class: 'list',
-    style: 'width: 300px;background-color: blue;'
-}, [
-        createElement('li', {
-            class: 'item',
-            'data-index': 0,
-        }, [
-            createElement('p', { class: 'text' }, ['第一个列表项'])
-        ]),
-        createElement('li', {
-            class: 'item',
-            'data-index': 1,
-        }, [
-            createElement('p', { class: 'text' }, [createElement('span', { class: 'title' }, ['第二个列表项'])])
-        ]),
-        createElement('li', {
-            class: 'item',
-            'data-index': 2,
-        }, ['第三个列表项']),
-    ]
-)
+import doPatch from './doPatch';
+
+const vDom1 = createElement('ul', { 
+    class: 'list', 
+    style: 'width: 300px; height: 300px; background-color: orange'
+  }, [
+    createElement('li', { 
+      class: 'item', 
+      'data-index': 0 
+    }, [
+      createElement('p', { 
+        class: 'text'
+      }, [
+        '第1个列表项'
+      ])
+    ]),
+    createElement('li', { 
+      class: 'item', 
+      'data-index': 1
+    }, [
+      createElement('p', { 
+        class: 'text'
+      }, [
+        createElement('span', { 
+          class: 'title' 
+        }, [])
+      ])
+    ]),
+    createElement('li', { 
+      class: 'item', 
+      'data-index': 2
+    }, [
+      '第3个列表项'
+    ])
+  ]);
+  
 const vDom2 = createElement('ul', {
-    class: 'list-warp',
-    style: 'width: 300px;height:300px;background-color: green;'
+  class: 'list-wrap',
+  style: 'width: 300px; height: 300px; background-color: orange'
 }, [
-        createElement('li', {
-            class: 'item',
-            'data-index': 0,
-        }, [
-            createElement('p', { class: 'title' }, ['特殊'])
-        ]),
-        createElement('li', {
-            class: 'item',
-            'data-index': 1,
-        }, [
-            createElement('p', { class: 'text' }, [])
-        ]),
-        createElement('div', {
-            class: 'item',
-            'data-index': 2,
-        }, ['第三个列表项']),
-    ]
-)
+  createElement('li', {
+    class: 'item',
+    'data-index': 0
+  }, [
+    createElement('p', {
+      class: 'title'
+    }, [
+      '特殊列表项'
+    ])
+  ]),
+  createElement('li', {
+    class: 'item',
+    'data-index': 1
+  }, [
+    createElement('p', {
+      class: 'text'
+    }, [])
+  ]),
+  createElement('div', {
+    class: 'item',
+    'data-index': 2
+  }, [
+    '第3个列表项'
+  ])
+]);
 
-const rDom = render(vDom);
+const rDom = render(vDom1);
+renderDOM(
+  rDom,
+  document.getElementById('app')
+);
 
-// console.log(rDom, '真实dom')
+const patches = domDiff(vDom1, vDom2);
 
-const patches = domDiff(vDom, vDom2);
-console.log('补丁包', patches)
+doPatch(rDom, patches);
 
-renderDOM(rDom, document.getElementById('app'))
-
-// console.log(vDom)
+console.log(patches);
